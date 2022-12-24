c2e42d02b3f7cbf20d2273edfdbec9f70602dc55
ARCHANA-KUMARI <ms.archanacusat2015@gmail.com>
2022-12-24 16:38:54 +0530

    Pattern 4 added

---
+++ app/src/main/java/com/example/dsausingkotlin/pattern/Pattern4.kt
@@ -0,0 +1,22 @@
+package com.example.dsausingkotlin.pattern
+
+import java.util.*
+
+/**
+ * @Author: Archana Kumari
+ * @Date: 24-12-2022
+ */
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    println("Enter terms:")
+    println()
+    var terms: Int = scanner.nextInt()
+    scanner.close()
+    for (i in 0..terms - 1) {
+        for (j in 0..i) {
+            print(terms - j)
+            print(" ")
+        }
+        println()
+    }
+}
